6fc2190be6c73049db39f865e3beaaa6572a80fd
Marc Miller <marc@bigoceanstudios.com>
2012-01-04 14:05:19 -0600

    Initial release

---
+++ system/third_party/dashee/language/english/lang.dashee.php
@@ -0,0 +1,93 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+	'dashee_module_name' =>
+	'dashEE',
+
+	'dashee_module_description' =>
+	'Fully customizable alternate CP dashboard.',
+
+	'module_home' => 'dashEE Home',
+
+	'dashee_term' 	=> 'Dashboard',
+	'dashee_settings' 	=> 'Settings',
+
+	'btn_collapse'	=> '- Collapse All',
+	'btn_expand'	=> '+ Expand All',
+	'btn_widgets'	=> 'Widgets',
+	'btn_settings'	=> 'Settings',
+	'btn_save'		=> 'Save Layout',
+
+	'capGeneral'	=> 'General',
+	'capLayouts'	=> 'Saved Layouts',
+	'capGroupLayouts'	=> 'Default Member Group Layouts',
+
+	'thPreference' 	=> 'Preference',
+	'thSetting' 	=> 'Setting',
+	'thName' 		=> 'Name',
+	'thDescription' => 'Description',
+	'thOptions'		=> 'Options',
+	'thMemberGroup' => 'Member Group',
+	'thLayout'		=> 'Layout',
+
+	'prefNumColumns'=> 'Number of columns?',
+	'prefReset'		=> 'Reset all user dashboards to the new defaults set above.',
+
+	'lblLayoutName' => 'Layout name',
+	'lblLayoutDesc' => 'Description',
+
+	'confRemoveWidget'  => 'Are you sure you want to remove this widget from your dashboard?',
+	'confDeleteLayout'  => 'Are you sure you want to delete this layout?',
+	'confLoadLayout'	=> '<strong>WARNING:</strong> this will reset your current dashboard and replace it with this saved layout.',
+
+	'default_layout' => 'Default layout',
+
+	'widget_added'	=> 'Widget added.',
+
+	'permission_denied' => 'You do not have permission to use this widget.',
+
+	'help_layouts' => '	<p>Saved layouts give EE Super Admins the ability to save dashboard configurations for later use or assign those configurations as the
+						default for users of a specific member group. Use the options below to <strong>load</strong> a saved layout to your own dashboard, mark a
+						new layout as <strong>default</strong> or <strong>delete</strong> obsolete saved layouts.</p><br />
+						<p>The default layout is used for new users accessing the dashboard for the first time with no other layout assigned.</p><br />
+						<p>The default member group layouts listed below allow you to assign specific saved layouts as the default for selected membership groups.
+						This allows you to create different dashboard layouts tailored for different member groups. <strong>Please note:</strong> the defaults
+						set here will only affect new users who are visiting the module for the first time. If they have already logged in and viewed the dashEE
+						module their existing configuration will remain the same.</p>',
+
+
+	// Widgets
+	'wgt_create_links_name' => 'EE Create Links',
+	'wgt_create_links_description' => 'EE create links from default control panel.',
+
+	'wgt_modify_links_name' => 'EE Modify Links',
+	'wgt_modify_links_description' => 'EE modify links from default control panel.',
+
+	'wgt_view_links_name' => 'EE View Links',
+	'wgt_view_links_description' => 'EE view links from default control panel.',
+
+	'wgt_recent_entries_name' => 'Recent Entries',
+	'wgt_recent_entries_description' => 'Displays 10 most recent EE entries.',
+
+	'wgt_recent_comments_name' => 'Recent Comments',
+	'wgt_recent_comments_description' => 'Displays 10 most recent EE entry comments.',
+
+	'wgt_welcome_name' => 'Welcome to dashEE',
+	'wgt_welcome_description' => 'General module info and links.',
+
+	'wgt_new_members_name' => 'New Members',
+	'wgt_new_members_description' => 'The last 10 members who have joined your site.',
+
+	'wgt_feed_reader_name' => 'RSS Feed Reader',
+	'wgt_feed_reader_description' => 'Display the 5 most recent posts from an RSS feed.',
+
+	'wgt_blank_name' => 'Blank Widget',
+	'wgt_blank_description' => 'Blank text widget, can be configured with whatever content you wish.',
+
+	'wgt_carthrob_orders_name' => 'Carthrob Orders',
+	'wgt_carthrob_orders_description' => 'Displays the 10 most recent Carthrob orders.',
+
+);
+
+/* End of file lang.dashee.php */
+/* Location: /system/expressionengine/third_party/dashee/language/english/lang.dashee.php */
---
+++ system/third_party/dashee/widgets/wgt.carthrob_orders.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * Recent Carthrob Entries Widget
+ *
+ * Display static listing of 10 most recent carthrob orders entries.
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Widget
+ * @author		Marc Miller
+ * @link		http://bigoceanstudios.com
+ */
+
+
+class Wgt_carthrob_orders
+{
+	public 	$title;
+	public $wclass;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->title = 'Recent Store Orders';
+		$this->wclass = 'contentMenu';
+
+		$this->EE =& get_instance();
+	}
+
+	// ----------------------------------------------------------------
+
+	/**
+	 * Index Function
+	 *
+	 * @return 	string
+	 */
+	public function index()
+	{
+		// get the Carthrob Orders channel from the DB
+		$ct_query = $this->EE->db->select('value')
+														->from('cartthrob_settings')
+														->where('cartthrob_settings.key', 'orders_channel')
+														->get();
+		if ($ct_query->num_rows() > 0)
+		{
+			$channel_id = $ct_query->row('value');
+		}
+
+		// get the Carthrob Orders Total Field from the DB
+		$ct_query = $this->EE->db->select('value')
+														->from('cartthrob_settings')
+														->where('cartthrob_settings.key', 'orders_total_field')
+														->get();
+		if ($ct_query->num_rows() > 0)
+		{
+			$total_id = $ct_query->row('value');
+		}
+
+		// get the Carthrob Customer Name Field from the DB
+		$ct_query = $this->EE->db->select('value')
+														->from('cartthrob_settings')
+														->where('cartthrob_settings.key', 'orders_customer_name')
+														->get();
+		if ($ct_query->num_rows() > 0)
+		{
+			$name_id = $ct_query->row('value');
+		}
+
+		// get the Carthrob Customer Email Field from the DB
+		$ct_query = $this->EE->db->select('value')
+														->from('cartthrob_settings')
+														->where('cartthrob_settings.key', 'orders_customer_email')
+														->get();
+		if ($ct_query->num_rows() > 0)
+		{
+			$email_id = $ct_query->row('value');
+		}
+
+		// get most recent 10 entries from DB
+		$entries = $this->EE->db->select('t.entry_id AS entry_id, t.title AS title, t.channel_id AS channel_id, t.entry_date AS entry_date, d.field_id_'.$total_id.' AS total, d.field_id_'.$name_id.' AS name, d.field_id_'.$email_id.' AS email')
+														->from('channel_titles t')
+														->join('channel_data d', 't.entry_id = d.entry_id', 'inner')
+														->where('t.channel_id', $channel_id)
+														->order_by('t.entry_date DESC')
+														->limit(10)
+														->get();
+		// generate table HTML
+		$display = '';
+		if($entries->num_rows() > 0)
+		{
+			foreach($entries->result() as $entry)
+			{
+				$display .= '
+					<tr class="'.alternator('odd','even').'">
+						<td><a href="'.BASE.AMP.'D=cp'.AMP.'C=content_publish'.AMP.'M=entry_form'.AMP.'channel_id='.$entry->channel_id.AMP.'entry_id='.$entry->entry_id.'">'.$entry->title.'</a></td>
+						<td>'.$entry->name.'</td>
+						<td>'.$entry->email.'</td>
+						<td>'.$entry->total.'</td>
+						<td>'.date('m/d/Y',$entry->entry_date).'</td>
+					</tr>';
+			}
+		}
+		else
+		{
+			$display = '<tr><td colspan="2"><center>No entries have been created.</center></td></tr>';
+		}
+
+		return '
+			<table>
+				<thead><tr><th>Order #</th><th>Customer</th><th>Email</th><th>Total</th><th>Order Date</th></tr></thead>
+				<tbody>'.$display.'</tbody>
+			</table>
+		';
+
+	}
+}
